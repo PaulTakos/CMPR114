d46db1b9bf11ec849deb4450b0cbbb2d65545a03
Paul <ptakemot@uci.edu>
2023-03-06 20:26:52 -0800

    Project 7 committed

---
+++ Quiz1/Quiz1_Proj7.py
@@ -0,0 +1,11 @@
+# Quiz 1 Project 7
+# Author: Paul Takemoto
+
+# initialize total value
+total = 0
+
+# Sums the numbers 1-10 through a for loop
+for num in range(1, 11):
+    total += num
+
+print('The sum of 1-10 is ', total)
