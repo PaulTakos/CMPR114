93b9625cbf2e5198f98cdaa442b5c0206250e5e4
Paul <ptakemot@uci.edu>
2023-03-16 16:53:49 -0700

    Initial commit (project 1)

---
+++ HW5/HW5_Proj1.py
@@ -0,0 +1,23 @@
+# HW 5 Proj 1
+# Author: Paul Takemoto
+
+def main():
+    value_actual = float(input('Enter actual property value: '))
+    value_assessment = get_assessment_value(value_actual)
+    tax_prop = get_property_tax(value_assessment)
+
+    print(f'Assessment value: ${value_assessment: ,.2f}')
+    print(f'Property tax: ${tax_prop: ,.2f}')
+
+# Function to calculate assessment value (60% of actual)
+def get_assessment_value(actual):
+    assessment = actual * 0.6
+    return assessment
+
+# Function to calculate property tax (0.72% of assessment)
+def get_property_tax(val):
+    tax = val * 0.0072
+    return tax
+
+# Calls main function
+main()
