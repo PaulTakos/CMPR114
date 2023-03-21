a2a83570880491f1aa3fc0e6e8e6016859e7d396
Paul <ptakemot@uci.edu>
2023-03-20 19:30:46 -0700

    Initial commit (project 1)

---
+++ Ex 6/Challenge1.py
@@ -0,0 +1,14 @@
+# Exercise 6 Challenge 1
+# Author: Paul Takemoto
+
+firstname = input('Enter your first name: ')
+lastname = input('Enter your last name: ')
+age = int(input('Enter your age: '))
+
+info_file = open('info.txt', 'w')
+info_file.write(firstname)
+info_file.write('\n' + lastname)
+info_file.write('\n' + str(age))
+
+info_file.close()
+print('\nInformation written to file \'info.txt\'.')
