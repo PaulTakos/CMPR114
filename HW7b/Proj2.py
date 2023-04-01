76efa2cca6654e5e5e941340dc28e7cf0ca9f46d
Paul <ptakemot@uci.edu>
2023-03-31 18:57:17 -0700

    Added comments (project 2)

--- HW7b/Proj2.py
+++ HW7b/Proj2.py
@@ -1,12 +1,14 @@
+# HW 7b Project 2
+# Paul Takemoto
 
 import pickle
 
 def main():
-    vegetables = {}  # Empty dictionary
+    vegetables = {}  # Creates an empty dictionary
     selection = 0
 
     end_of_file = False
-    veg_file1 = open('vegetables.dat', 'rb')
+    veg_file1 = open('vegetables.dat', 'rb')  # Opens a .dat file for binary reading
 
     # Reads file to the end
     while not end_of_file:
@@ -17,12 +19,12 @@ def main():
             # Sets flag to indicate end of file
             end_of_file = True
 
-    veg_file1.close()
+    veg_file1.close()  # Closes file
 
-    while selection != 5:
-        selection = get_menu_choice()
+    while selection != 5:  # As long as user doesn't quit program (option 5)
+        selection = get_menu_choice()  # Gets user's selection via function
 
-        # Processes user's choice
+        # Processes user's choice based on selection (calls appropriate function)
         if selection == 1:
             show_list(vegetables)
         elif selection == 2:
@@ -33,11 +35,12 @@ def main():
             delete(vegetables)
 
     # Pickles the vegetable dictionary into a file
-    veg_file2 = open('vegetables.dat', 'wb')
+    veg_file2 = open('vegetables.dat', 'wb')  # Opens a .dat file for binary writing
     pickle.dump(vegetables, veg_file2)
     veg_file2.close()
 
 def get_menu_choice():
+    # Prints menu with choices, and prompts user for input
     print('Vegetables and Their Prices')
     print('1. Show vegetable list')
     print('2. Add new vegetable')
@@ -45,18 +48,21 @@ def get_menu_choice():
     print('4. Delete an existing vegetable')
     print('5. Quit the program')
     selection = int(input('Enter your choice: '))
-    while selection < 1 or selection > 5:
+
+    while selection < 1 or selection > 5:  # Ensures selection is valid
         print('Selection must be a number between 1-5')
         selection = int(input('Enter a valid choice: '))
     return selection
 
 def show_list(vegetables):
+    # Displays dictionary contents (key and value)
     print('\nVegetables and Their Prices')
     for key, value in vegetables.items():
         print(key + '  $' + value)
     print()
 
 def add(vegetables):
+    # Asks user to enter new vegetable and price to add
     name = input('Enter a vegetable: ')
     price = float(input('Enter its price: '))
 
@@ -68,9 +74,10 @@ def add(vegetables):
     print()
 
 def change(vegetables):
+    # Asks user to enter a vegetable to change its price
     name = input('Enter a vegetable: ')
 
-    if name in vegetables:
+    if name in vegetables:  # Checks to ensure vegetable is in dictionary
         # Get a new price
         price = float(input('Enter a new price: '))
 
@@ -81,12 +88,15 @@ def change(vegetables):
     print()
 
 def delete(vegetables):
+    # Asks user to enter a vegetable to delete
     name = input('Enter a vegetable: ')
 
+    # Ensures vegetable is in dictionary, then deletes it
     if name in vegetables:
         del vegetables[name]
     else:
         print('That vegetable is not found.')
     print()
 
+# Calls main function
 main()
