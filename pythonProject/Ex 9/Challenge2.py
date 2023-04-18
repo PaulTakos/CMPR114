dd724c12c4381f2444acaab09110fde9b293b484
Paul <ptakemot@uci.edu>
2023-04-18 14:54:07 -0700

    Initial commit (Project 2)

---
+++ pythonProject/Ex 9/Challenge2.py
@@ -0,0 +1,20 @@
+# Exercise 9 Challenge 2
+# Paul Takemoto
+
+class Teacher:
+    # using init to create customized constructor with 3 arguments
+    def __init__(self, name, classroom, course):
+        self.name = name
+        self.classroom = classroom
+        self.course = course
+
+    def GetProfessor(self):
+        print('Name: ' + self.name)
+        print('Class: ' + self.classroom)
+        print('Course: ' + self.course)
+
+Teacher1 = Teacher('Prof. Sim', 'A206', 'Python Programming')
+Teacher2 = Teacher('Prof. Huynh', 'A223', 'Python Programming')
+
+Teacher1.GetProfessor()
+Teacher2.GetProfessor()
