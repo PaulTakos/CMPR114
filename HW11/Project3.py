5edb8d81760d8e55f3959cd373d8ad9c5c725575
Paul <ptakemot@uci.edu>
2023-04-22 14:48:24 -0700

    Initial commit (project 3)

---
+++ HW11/Project3.py
@@ -0,0 +1,20 @@
+# HW11 Project 3
+# Paul Takemoto
+
+import Retail_data
+
+def main():
+    item1 = Retail_data.RetailItem('Jacket', 12, 59.95)
+    item2 = Retail_data.RetailItem('Designer jeans', 40, 34.95)
+    item3 = Retail_data.RetailItem('Shirt', 20, 24.95)
+
+    print('Item 1: ' + item1.get_description() + ', $' + str(item1.get_price()))
+    print('Quantity: ' + str(item1.get_quantity()) + '\n')
+
+    print('Item 2: ' + item2.get_description() + ', $' + str(item2.get_price()))
+    print('Quantity: ' + str(item2.get_quantity()) + '\n')
+
+    print('Item 3: ' + item3.get_description() + ', $' + str(item3.get_price()))
+    print('Quantity: ' + str(item3.get_quantity()) + '\n')
+
+main()
---
+++ HW11/Retail_data.py
@@ -0,0 +1,16 @@
+# RetailItem class
+
+class RetailItem:
+    def __init__(self, description, quantity, price):
+        self.__description = description
+        self.__quantity = quantity
+        self.__price = price
+
+    def get_description(self):
+        return self.__description
+
+    def get_quantity(self):
+        return self.__quantity
+
+    def get_price(self):
+        return self.__price
